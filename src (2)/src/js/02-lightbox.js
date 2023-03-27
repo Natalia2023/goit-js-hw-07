5509de33f2aca013be1a373728a3ebef09932248
Nataliy_Anohina2023 <117434665+Natalia2023@users.noreply.github.com>
2023-03-27 22:17:53 +0300

    setting 2

--- src (2)/src/js/02-lightbox.js
+++ src (2)/src/js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryMarkup = document.querySelector('.gallery');
+
+const galleryUsage = galleryItems//витягую елем із галереї через деструкторизацію
+.map(({preview, original, description}) => {//створюю новий масив значень через шабл рядок
+return `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+} )
+
+.join('');//повертаю рядок в одну стрічку
+galleryMarkup.insertAdjacentHTML("beforeend", galleryUsage);//вставляю елементи в галерею
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', // додаю відображення підписів до зображень з атрибута alt
+    captionDelay: 250,//підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення
+  });
 
 console.log(galleryItems);
